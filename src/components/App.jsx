4c7cd7684a26ad297dd9bdd5565d9efae07a02ab
Cristina Pérez <cristinaperezhuerta@gmail>
2025-06-03 12:58:42 +0200

    using filters to search lists

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,56 @@
 import "../styles/App.scss";
 import data from "../data/adalabers.json";
 import Header from "./Header";
-import Form from "./Form";
 import TableAdalabers from "./TableAdalabers";
 import { useState } from "react";
 
 function App() {
   const [adalabers] = useState(data.results);
+  const [nameFilter, setNameFilter] = useState("");
+  const [counselorFilter, setCounselorFilter] = useState("");
+
+  const handleInputName = (ev) => {
+    setNameFilter(ev.target.value);
+  };
+  const handleInputCounselor = (ev) => {
+    setCounselorFilter(ev.target.value);
+  };
+
+  const filteredAdalabers = adalabers
+    .filter((eachAdalaber) =>
+      eachAdalaber.name
+        .toLocaleLowerCase()
+        .includes(nameFilter.trim().toLocaleLowerCase())
+    )
+    .filter((eachAdalaber) => eachAdalaber.counselor.includes(counselorFilter));
 
   return (
     <div>
       <Header />
       <main>
-        <Form />
-        <TableAdalabers adalabers={adalabers} />
+        <form>
+          <label htmlFor="nameFilter">Filtrar por nombre:</label>
+          <input
+            onInput={handleInputName}
+            type="text"
+            name="nameFilter"
+            id="nameFilter"
+            placeholder="Ej: Nerea"
+          />
+          <label htmlFor="counselorFilter">Elije una Tutora</label>
+          <select
+            onInput={handleInputCounselor}
+            name="counsolerFilter"
+            id="counselorFilter"
+          >
+            <option selected disabled>
+              -- Selecciona tu profesora --
+            </option>
+            <option value="Dayana">Profesora Dayana</option>
+            <option value="Iván">Profesor Iván</option>
+          </select>
+        </form>
+        <TableAdalabers adalabers={filteredAdalabers} />
       </main>
     </div>
   );
